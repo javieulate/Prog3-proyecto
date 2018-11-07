c662cb66063a522604901cd7bebe6eea2715fc94
juansolozabal <juan.solozabal@opendeusto.es>
2018-11-07 10:50:04 +0100

    New files for LN and LP added

---
+++ src/LN/clsUsuario.java
@@ -0,0 +1,12 @@
+package LN;
+
+public class clsUsuario {
+
+	
+	private String nomusuario;
+	private String mail;
+	private String contraseña;
+	private int numerovidas;
+	private int puntuacion;
+
+}
