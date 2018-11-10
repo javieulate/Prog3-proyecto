cd5dce5fc627656d0728b4d94eabb8c8248a7540
javieulate <javier.a.eulate@opendeusto.es>
2018-11-10 22:34:25 +0100

    New files added

--- src/LN/clsGestor.java
+++ src/LN/clsGestor.java
@@ -1,15 +1,20 @@
 package LN;
 
-
-
+import java.io.IOException;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.regex.Pattern;
 
+import COMUN.clsElementoRepetido;
+import COMUN.clsUsuarioNoRegistrado;
+import COMUN.clsConstantes.enFicDatos;
+import LD.clsDatos;
+import LD.itfDatos;
 
 public class clsGestor {
 	
-	public static void altaUsuario(clsUsuario usuario,  ArrayList <clsUsuario> listaUsu)throws clsUsuarioRepetido{
+	public static void altaUsuario(clsUsuario usuario,  ArrayList <clsUsuario> listaUsu) throws clsUsuarioRepetido {
 		clsUsuario a = new clsUsuario();
 		a=usuario;
 		HashSet<clsUsuario>miSet = new HashSet<clsUsuario>();
@@ -24,6 +29,64 @@ public class clsGestor {
 			throw new clsUsuarioRepetido();
 		}
 	}
+	
+	public static ArrayList<clsUsuario> ListaUsuariosclsUsuarios()
+	{
+		ArrayList<Serializable>leido;
+		ArrayList<clsUsuario>retorno;
+		clsDatos objDatos;
+		
+		retorno=null;
+		objDatos=new clsDatos();
+		
+		try
+		{
+			objDatos.ComenzarRead(enFicDatos.FICHEROUSU);
+			leido=objDatos.Read();
+			retorno=new ArrayList<clsUsuario>();
+			
+			for(Serializable s: leido)
+			{
+				retorno.add((clsUsuario)s);
+			}
+			
+		}
+		catch(IOException e)
+		{
+			//Si hay error devuelvo una lista vacía.
+			retorno=new ArrayList<clsUsuario>();
+		}
+		finally
+		{
+			objDatos.TerminarRead();
+		}
+		
+		objDatos=null;
+		return retorno;
+	}	
+	/**
+	 * Método para comprobar si un usuario existe en fichero.	
+	 * @param nomusuario nombre de usuario a buscar
+	 * @param correoelec correo del usuario a buscar
+	 * @throws clsUsuarioNoRegistrado excepción que salta si no existe
+	 */
+	public static void ComprobarUsuario(String nomusuario, String correoelec) throws clsUsuarioNoRegistrado
+		{
+			boolean marca = true;
+			ArrayList<clsUsuario>listausuarios = ListaUsuariosclsUsuarios();
+			
+			for (clsUsuario aux : listausuarios) 
+			{
+				if(aux.getMail().toUpperCase().equals(correoelec.toUpperCase()) && aux.getNomUsuario().toUpperCase().equals(nomusuario.toUpperCase()))
+				{
+					marca = false;
+				}
+			}
+			if (marca == true)
+			{
+				throw new clsUsuarioNoRegistrado();
+			}
+		}
 	 public static boolean validarEmail(String email) throws clsMailNoValido
 	  {
 		  Pattern pattern = Pattern.compile(clsUsuario.EMAIL_ADDRESS); 
@@ -37,4 +100,142 @@ public class clsGestor {
 			  throw new clsMailNoValido();
 		  }
 	  }
+
+/**
+		 * Método usado para guardar los datos en fichero sesion del usuario que ha inciado su sesión en la 
+		 * aplicación.
+		 * @param nomusuario nombre del usuario
+		 * @param correoelec correo del usuario
+		 */
+		public static void IniciarSesion(String nomusuario, String correoelec)
+			{
+				
+				ArrayList<clsUsuario>listaususarios = ListaUsuariosclsUsuarios();
+				clsUsuario a = null;
+				
+				for (clsUsuario aux : listaususarios) 
+				{
+					if(aux.getMail().toUpperCase().equals(correoelec.toUpperCase()) && aux.getNomUsuario().toUpperCase().equals(nomusuario.toUpperCase()))
+					{
+						a = aux;
+					}
+				}
+			
+		
+				itfDatos ObjDatos = new clsDatos();
+					
+				ObjDatos.ComenzarSave(enFicDatos.FICHEROSESION);
+				ObjDatos.Save((Serializable)a);
+				ObjDatos.TerminarSave();
+				
+			}
+		
+		/**
+		 * Este es el método del clsGestor que elabora una lista de usuarios de nuestra aplicación. Lo que hace es
+		 * leer toda la información que existe de los usuarios registrados entrando en el fichero de estos. Mete los
+		 * usuarios registrados en una lista y la manda a donde queramos, normalmente se usa en el menú principal.
+		 * 
+		 * @return lista llena de información de los usuarios.
+		 */
+
+		public static ArrayList<itfProperty> ListaUsuarios()
+		{
+			ArrayList<Serializable>leido;
+			ArrayList<itfProperty>retorno;
+			clsDatos objDatos;
+			
+			retorno=null;
+			objDatos=new clsDatos();
+			
+			try
+			{
+				objDatos.ComenzarRead(enFicDatos.FICHEROUSU);
+				leido=objDatos.Read();
+				retorno=new ArrayList<itfProperty>();
+				
+				for(Serializable s: leido)
+				{
+					retorno.add((itfProperty)s);
+				}
+				
+			}
+			catch(IOException e)
+			{
+				//Si hay error devuelvo una lista vacía.
+				retorno=new ArrayList<itfProperty>();
+			}
+			finally
+			{
+				objDatos.TerminarRead();
+			}
+			
+			objDatos=null;
+			return retorno;
+		}
+		
+		/**
+		 * Este es el método de alta de un usuario. Es un método que usa la lógica de presentación cuando quiere registrar
+		 * un usuario nuevo en nuestra aplicación. Dispone de la funcionalidad del Hashset para eliminar aquellos usuarios 
+		 * que estén repetidos (por mail). Los guarda en el fichero de usuarios que hemos definido en clsConstantes.
+		 * @param nombre nombre del usuario
+		 * @param apellido apellido del usuario
+		 * @param correoelectronico mail del usuario
+		 * @param nomusu nombre de usuario
+		 */
+		public static void AltaUsuario(String nombre, String apellido, String mail, String nomUsu, String contrasena) throws clsElementoRepetido
+		{
+				itfProperty a = new clsUsuario(nombre, apellido, mail, nomUsu, contrasena);
+				itfDatos ObjDatos = new clsDatos();
+				
+				HashSet<itfProperty>miSet = new HashSet<itfProperty>();
+				
+				ArrayList<itfProperty>milista;
+				milista = ListaUsuarios();
+			
+				
+				for (itfProperty aux : milista) 
+				{
+					miSet.add((clsUsuario)aux);
+				}
+				if(miSet.add(a)==true)
+				{
+					ObjDatos.ComenzarSave(enFicDatos.FICHEROUSU);
+					ObjDatos.Save((Serializable)a);
+					ObjDatos.TerminarSave();
+					
+					ObjDatos.ComenzarSave(enFicDatos.FICHEROSESION);
+					ObjDatos.Save((Serializable)a);
+					ObjDatos.TerminarSave();
+				}
+				else
+				{
+					throw new clsElementoRepetido();
+				}		
+		}
+		
+		/**
+		 * Método que borra el fichero sesion cuando el usuario abandona la aplicación.
+		 */
+		public static void CerrarSesion()
+			{
+				itfDatos ObjDatos = new clsDatos();
+				
+				ObjDatos.ResetFile(enFicDatos.FICHEROSESION);
+			}
+		
+		public static String NomUsuario() throws IOException
+		{
+			String nomusu ="";
+			itfProperty b = null;
+			itfDatos ObjDatos = new clsDatos();
+			
+			ObjDatos.ComenzarRead(enFicDatos.FICHEROSESION);
+			b =  (itfProperty)ObjDatos.ReadSerializable();
+			ObjDatos.TerminarRead();
+			
+			nomusu = b.getStringProperty(COMUN.clsConstantes.PROP_USUARIO_NOMUSUARIO);
+			
+			return nomusu;
+		}
+		
 }
--- src/LN/clsUsuario.java
+++ src/LN/clsUsuario.java
@@ -1,40 +1,62 @@
 package LN;
 
-
+import static COMUN.clsConstantes.*;
 
 import java.io.Serializable;
 
-public class clsUsuario implements Serializable {
+import COMUN.clsPropertyException;
 
+public class clsUsuario implements Serializable, itfProperty {
 	
-	private String nomUsuario;
+	private String nombre;
+	private String apellido;
 	private String mail;
-	private String contraseña;
+	private String nomUsuario;
+	private String contrasena;
 	private int numeroVidas;
 	private int puntuacion;
 	
 	public clsUsuario(){
 		nomUsuario="";
 		mail="";
-		contraseña="";
+		contrasena="";
 		numeroVidas=5;
 		puntuacion=0;				
 	}
 	
-	public clsUsuario(String nomUsuario, String mail, String contraseña, int numeroVidas, int puntuacion){
-		this.nomUsuario=nomUsuario;
-		this.mail=mail;
-		this.contraseña=contraseña;
-		this.numeroVidas=numeroVidas;
-		this.puntuacion=puntuacion;
-	}
-
-	public String getnomUsuarioarioario() {
-		return nomUsuario;
-	}
-
-	public void setnomUsuarioarioario(String nomUsuario) {
+//	public clsUsuario(String nomUsuario, String mail, String contrasena, int numeroVidas, int puntuacion){
+//		this.nomUsuario=nomUsuario;
+//		this.mail=mail;
+//		this.contrasena=contrasena;
+//		this.numeroVidas=numeroVidas;
+//		this.puntuacion=puntuacion;
+//	}
+	
+	public clsUsuario(String nombre, String apellido, String mail,
+			String nomUsuario, String contrasena) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.mail = mail;
 		this.nomUsuario = nomUsuario;
+		this.contrasena = contrasena;
+	}
+
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
 	}
 
 	public String getMail() {
@@ -45,12 +67,20 @@ public class clsUsuario implements Serializable {
 		this.mail = mail;
 	}
 
-	public String getContraseña() {
-		return contraseña;
+	public String getNomUsuario() {
+		return nomUsuario;
 	}
 
-	public void setContraseña(String contraseña) {
-		this.contraseña = contraseña;
+	public void setNomUsuario(String nomUsuario) {
+		this.nomUsuario = nomUsuario;
+	}
+
+	public String getContrasena() {
+		return contrasena;
+	}
+
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
 	}
 
 	public int getNumeroVidas() {
@@ -69,8 +99,10 @@ public class clsUsuario implements Serializable {
 		this.puntuacion = puntuacion;
 	}
 
+	public static String getEmailAddress() {
+		return EMAIL_ADDRESS;
+	}
 
-	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -110,10 +142,72 @@ public class clsUsuario implements Serializable {
 		return true;
 	}
 
-
-
 	public static final String EMAIL_ADDRESS = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
 	            + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-	
 
+	@Override
+	public String getStringProperty(String propiedad) throws clsPropertyException {
+		
+		String retorno="";
+		
+		switch(propiedad)
+		{
+			case PROP_USUARIO_NOMBRE:
+				retorno= this.getNombre();
+			break;
+			case PROP_USUARIO_APELLIDO:
+				retorno= this.getMail();
+			break;
+			case PROP_USUARIO_NOMUSUARIO:
+				retorno= this.getNomUsuario();
+			break;
+			case PROP_USUARIO_CORREOELEC:
+				retorno= this.getMail();
+			break;
+			case PROP_USUARIO_CONTRASENA:
+				retorno= this.getContrasena();
+			break;
+				
+			default : throw new clsPropertyException(propiedad);
+		}	
+		return retorno;
+		
+		
+	}
+
+	@Override
+	public Integer getIntegerProperty(String propiedad) throws clsPropertyException {
+		
+	int retorno=0;
+		
+		switch(propiedad)
+		{
+			case PROP_USUARIO_NUMEROVIDAS:
+				retorno= this.getNumeroVidas();
+			break;
+			case PROP_USUARIO_PUNTUACION:
+				retorno= this.getPuntuacion();
+			break;
+				
+			default : throw new clsPropertyException(propiedad);
+		}	
+		return retorno;
+		
+		
+	}
+
+	@Override
+	public Float getFloatProperty(String propiedad) throws clsPropertyException {
+		throw new clsPropertyException(propiedad);
+	}
+
+	@Override
+	public Double getDoubleProperty(String propiedad) throws clsPropertyException {
+		throw new clsPropertyException(propiedad);
+	}
+
+	@Override
+	public char getCharProperty(String propiedad) {
+		throw new clsPropertyException(propiedad);
+	}
 }
